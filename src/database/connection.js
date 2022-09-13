1452216ed670521a819edb3aae41acd78c63dc2a
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2022-09-12 19:06:13 -0500

    first commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,10 +6,10 @@ export const dbSettings = {
   password: config.dbPassword,
   server: config.dbServer,
   database: config.dbDatabase, */
-  user: 'sa',
-  password: 'Solomon2011',
-  server: '181.198.20.179',
-  database: 'PRODUCCION',
+  user: "sa",
+  password: "Solomon2011",
+  server: "181.198.20.179",
+  database: "PRODUCCION",
   options: {
     encrypt: true, // for azure
     trustServerCertificate: true, // change to true for local dev / self-signed certs
@@ -17,15 +17,10 @@ export const dbSettings = {
 };
 
 export const getConnection = async () => {
-  const pore = process.env.PORT;
   try {
-    console.log("puerto 1:"+pore);
-    console.log(dbSettings);
     const pool = await sql.connect(dbSettings);
     return pool;
   } catch (error) {
-    console.log("puerto error:"+pore);
-    console.log(dbSettings);
     console.error(error);
   }
 };
